8046ad7803ff7e7ad5bac3d5818811713c34af30
Leon Ramos <nemean.dev@gmail.com>
2024-11-25 16:58:45 +0100

    Update models to include 'Post' class / 'post' table for user posts.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
@@ -13,5 +14,21 @@ class User(db.Model):
     lname: orm.Mapped[Optional[str]] = orm.mapped_column(sa.String(128))
     password_hash: orm.Mapped[str] = orm.mapped_column(sa.String(256))
 
+    posts: orm.WriteOnlyMapped['Post'] = orm.relationship(
+        back_populates='author')
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+class Post(db.Model):
+    id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
+    body: orm.Mapped[str] = orm.mapped_column(sa.String(600))
+    timestamp: orm.Mapped[datetime] = orm.mapped_column(index= True, 
+                                                        default= lambda: datetime.now(timezone.utc))
+    user_id: orm.Mapped[int] = orm.mapped_column(sa.ForeignKey(User.id),
+                                                 index=True)
+    
+    author: orm.Mapped[User] = orm.relationship(back_populates='posts')
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
