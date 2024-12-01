42c8d1ebbe8672c03515d08c378f72137ff7b167
Leon Ramos <nemean.dev@gmail.com>
2024-12-01 14:30:01 +0100

    Custom error pages. db session rollback on 500 errors.

--- app/__init__.py
+++ app/__init__.py
@@ -11,4 +11,4 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login' # on views with @login_required; redirect to 'login' and come back once logged in.
 
-from app import routes, models
+from app import routes, models, errors
