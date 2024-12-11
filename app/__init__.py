df5ea90aa402f63533557bc44c387e626b1d58a9
Leon Ramos <nemean.dev@gmail.com>
2024-12-11 17:15:39 +0100

    Install 'flask-moment' wrapper of 'Moment.js' to display different timezones and time formats according to the user's local configuration. Add Moment tag to base template.

--- app/__init__.py
+++ app/__init__.py
@@ -5,16 +5,19 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from config import Config
 from flask_mail import Mail
+from flask_moment import Moment
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login' # on views with @login_required; redirect to 'login' and come back once logged in.
 mail = Mail(app)
+moment = Moment(app)
 
 # logging setup
 if not app.debug:
