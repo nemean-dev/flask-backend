c99b775b1258e3ccb2f09452b9e11c6bc16878a3
Leon Ramos <nemean.dev@gmail.com>
2024-12-12 16:17:34 +0100

    Mark templates for translation. Also mark the login message from flask_login in app/__init__

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_moment import Moment
-from flask_babel import Babel
+from flask_babel import Babel, lazy_gettext as _l
 from config import Config
 
 # for Babel
@@ -21,6 +21,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login' # on views with @login_required; redirect to 'login' and come back once logged in.
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 moment = Moment(app)
 babel = Babel(app, locale_selector=get_locale)
