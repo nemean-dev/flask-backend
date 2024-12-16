8b8bc4eed841ec86d05c1a5b3ddfbb4ad485d562
Leon Ramos <nemean.dev@gmail.com>
2024-12-16 15:45:22 +0100

    Restructure app: create Flask blueprint for 'errors' and move relevant files there.

--- app/__init__.py
+++ app/__init__.py
@@ -27,6 +27,9 @@ mail = Mail(app)
 moment = Moment(app)
 babel = Babel(app, locale_selector=get_locale)
 
+from app.errors import bp as errors_bp # Avoid circular dependencies
+app.register_blueprint(errors_bp)
+
 # logging setup
 if not app.debug:
     # send server errors by email
@@ -57,4 +60,4 @@ if not app.debug:
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')    
 
-from app import routes, models, errors
+from app import routes, models
